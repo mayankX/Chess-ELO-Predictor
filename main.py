6a2b91f738c2bce5d49c297f373c64c58f08f45a
Mayank Tiwari <mayanktiwari4396@gmail.com>
2019-11-12 18:55:55 -0500

    Misc package fix

--- main.py
+++ main.py
@@ -4,60 +4,6 @@ import chess.pgn
 import chess.engine
 
 
-def convertLetterToNumber(letter):
-    if letter == 'K':
-        return '100000000000'
-    if letter == 'Q':
-        return '010000000000'
-    if letter == 'R':
-        return '001000000000'
-    if letter == 'B':
-        return '000100000000'
-    if letter == 'N':
-        return '000010000000'
-    if letter == 'P':
-        return '000001000000'
-    if letter == 'k':
-        return '000000100000'
-    if letter == 'q':
-        return '000000010000'
-    if letter == 'r':
-        return '000000001000'
-    if letter == 'b':
-        return '000000000100'
-    if letter == 'n':
-        return '000000000010'
-    if letter == 'p':
-        return '000000000001'
-    if letter == '1':
-        return '000000000000'
-    if letter == '2':
-        return '000000000000000000000000'
-    if letter == '3':
-        return '000000000000000000000000000000000000'
-    if letter == '4':
-        return '000000000000000000000000000000000000000000000000'
-    if letter == '5':
-        return '000000000000000000000000000000000000000000000000000000000000'
-    if letter == '6':
-        return '000000000000000000000000000000000000000000000000000000000000000000000000'
-    if letter == '7':
-        return '000000000000000000000000000000000000000000000000000000000000000000000000000000000000'
-    if letter == '8':
-        return '000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000'
-    if letter == '/':
-        return ''
-
-
-def convertToBB(board):
-    bitBoard = ''
-    board = str(board.fen()).split(' ')[0]
-    for letter in board:
-        bitBoard = bitBoard + convertLetterToNumber(letter)
-    # bitBoard = bitBoard[1:-1]
-    return bitBoard
-
-
 def indefinite_analysis(engine):
     with engine.analysis(chess.Board()) as analysis:
         for info in analysis:
--- move_analysis_pipeline.py
+++ move_analysis_pipeline.py
@@ -9,15 +9,14 @@ import chess.engine
 import chess.pgn
 import chess.svg
 
-from main import convertToBB
-from models.game import *
+from models.move_analysis import *
 from util import *
 
 '''
 Loading the Logging library here
 '''
 # logging.basicConfig(level=logging.INFO, filename='app.log', filemode='w', format='%(asctime)s - %(levelname)s - %(message)s')
-with open('../logging_config.yaml', 'r') as f:
+with open('./logging_config.yaml', 'r') as f:
     config = yaml.safe_load(f.read())
     logging.config.dictConfig(config)
 logger = logging.getLogger(__name__)
@@ -25,7 +24,7 @@ logger = logging.getLogger(__name__)
 '''
 Connecting to Mongo DB
 '''
-config = load_config('../elo_config.yaml')
+config = load_config('./elo_config.yaml')
 profile = config["profile"]
 logger.info(f'Loaded configuration for Profile: {profile}')
 init_database(profile, config)
@@ -45,8 +44,6 @@ def parse_pgn(dirname: str, filename: str):
     Loading the chess processing engine
     '''
     engine = chess.engine.SimpleEngine.popen_uci("stockfish")
-    limit = chess.engine.Limit(time=0.200)
-    # limit = chess.engine.Limit(depth=18)
 
     '''
     Beginning to parse the files and load it into the warehouse
@@ -59,20 +56,17 @@ def parse_pgn(dirname: str, filename: str):
             break
 
         headers = game.headers
-        # print(headers)
 
         site = headers.get("Site")
         logging.info(f'Processing [{counter}] game... ID: {site}')
-        # if Game.site_exists(site=site):
-        if Game.objects(site=site).count() != 0:
-            logger.info("-> Skipping Already Processed File...")
+
+        gameMetadata = GameMetadata.get_by_id(site)
+        if gameMetadata is None:
+            gameMetadata = GameMetadata(headers, PGNMetadata())
             continue
 
-        dbGameObject = Game(headers, Metadata(counter, filename))
-        # for k, v in headers.items():
-        #     print(f'{k} -> {v}')
+        # dbGameObject = Game(headers, Metadata(counter, filename))
 
-        prev_eval = 0
         board = chess.Board()
         moves = list(game.mainline_moves())
         totalMoves = len(moves)
@@ -80,57 +74,27 @@ def parse_pgn(dirname: str, filename: str):
             board.push(move)
             uci = move.uci()
             print(f'{index + 1}/{totalMoves} Analyzing Move {uci} for Game: {counter}...')
-            result = engine.analyse(board, limit)
-
-            evaluationVal = result.score.relative
-            isMate = evaluationVal.is_mate()
-            if not isMate:
-                evaluation = evaluationVal.cp
-            else:
-                evaluation = evaluationVal.mate()
 
             turn = board.turn
-            # if turn:
-            #     diff = prev_eval - evaluation
-            #     if diff > 0.3:
-            #         evaluation_label = "B"  # badmove
-            #     else:
-            #         evaluation_label = "G"  # goodmove
-            # else:
-            #     evaluation *= -1
-            #     diff = evaluation - prev_eval
-            #     if diff > 0.3:
-            #         evaluation_label = "B"  # badmove
-            #     else:
-            #         evaluation_label = "G"  # goodmove
-
             encodedMove = convertToBB(board)
             fen = board.fen().__str__()
-            dbGameObject.moves.append(Move(uci, fen, encodedMove, evaluation, prev_eval, turn, isMate))
-
-            # print(f'DIFF: {diff}, Eval: {evaluation}, Prev: {prev_eval}, Label: {"Bad" if evaluation_label == "B" else "Good"}')
-            prev_eval = evaluation
 
         logger.info(f'Finished processing [{counter}] game... Saving outcome to database...')
-        dbGameObject.save(cascade=True)
+        # dbGameObject.save(cascade=True)
 
     engine.quit()
 
 
-# parse_pgn('lichess_db_standard_rated_2014-01.pgn')
-def parse_all_pgn(location='../data'):
-    logger.info(f'Parsing PGN files from Location: {location} for Game Analysis...')
+def parse_all_pgn(location='./data'):
+    logger.info(f'Parsing PGN files from Location: {location} for Move Analysis...')
     for dirname, _, filenames in os.walk(location):
         for filename in filenames:
             if filename.endswith('.pgn'):
-                # filePath = os.path.join(dirname, filename)
-                # print(filePath)
                 parse_pgn(dirname, filename)
 
 
-# parse_all_pgn('./data')
 '''
 from mongo_pipeline import *
-parse_all_pgn('../data')
+parse_all_pgn('./data')
 '''
-parse_all_pgn()
+# parse_all_pgn()
