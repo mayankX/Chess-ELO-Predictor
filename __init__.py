a2667b650257373e70ed75d47495e9406902c3e3
Mayank Tiwari <mayanktiwari4396@gmail.com>
2019-11-12 21:58:34 -0500

    Misc

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 __author__ = 'Mayank Tiwari'
 
-# import os, sys
-# ROOT_DIR = os.path.abspath(os.path.join(os.path.split(__file__)[0], '..'))
-# sys.path.append(ROOT_DIR)
+import os, sys
+ROOT_DIR = os.path.abspath(os.path.join(os.path.split(__file__)[0], '..'))
+sys.path.append(ROOT_DIR)
